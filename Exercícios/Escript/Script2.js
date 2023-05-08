be9ab7c80d9d075c4ab2b6064111e0adb5b351c4
Erik M B Freitas <122630310+Erik-Magno@users.noreply.github.com>
2023-05-07 23:09:43 -0300

    melhorias na div no css

--- Exercícios/Escript/Script2.js
+++ Exercícios/Escript/Script2.js
@@ -2,15 +2,18 @@ function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     let img = document.getElementById('gif')
+    let campo = document.getElementById('campo')
     if(num.value.length == 0) {
         alert('Por favor, digite um número!')
     }else {        
         img.style.display = 'none'
+        campo.style.display = 'block'
         var n = Number(num.value)
         tab.innerHTML = ''
         for(let c = 1; c <= 10; c++){
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`//para outras liguagens de programção...
             tab.appendChild(item)
             
         }
